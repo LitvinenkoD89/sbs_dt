02040c3414d6749c0bed468fc25768e56e1ff3eb
LitvinenkoD89 <torerolda@gmail.com>
2017-06-02 08:03:02 +0300

    услуги

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about_us
+  end
+  
+  def contacts
+  end  
+
+  def services
+  end
+
+  def reviews
+  end
+
+  def gallery
+  end  
+
+  def video
+  end  
+
+  def blog
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "home#index"
+  get "about_us" => "home#about_us"
+  get "contacts" => "home#contacts"
+  get "services" => "home#services"
+  get "reviews" => "home#reviews"
+  get "gallery" => "home#gallery"
+  get "video" => "home#video"
+  get "blog" => "home#blog"
 end
