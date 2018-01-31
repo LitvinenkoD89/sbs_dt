ba8839692ee4ea4814f0b1d1701d116608274a8d
LitvinenkoD89 <torerolda@gmail.com>
2018-02-01 00:47:54 +0300

    crtf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  # protect_from_forgery with: :null_session
-  skip_before_filter :verify_authenticity_token
-end
+  protect_from_forgery with: :null_session, if: ->{request.format.json?}
+end  
