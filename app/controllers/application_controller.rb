2e69394d0f60c9facda3148c79a01f8e0076ee3a
LitvinenkoD89 <torerolda@gmail.com>
2018-02-01 00:09:36 +0300

    crf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: false
+  protect_from_forgery with: :exception
+
+  # protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
 end
