6f460ad370db7832113bfc9b5b80bc4dd1594ba0
LitvinenkoD89 <torerolda@gmail.com>
2018-02-01 00:40:33 +0300

    crtf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
-
-  protect_from_forgery with: :null_session
-  # skip_before_filter :verify_authenticity_token
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  # protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
 end
