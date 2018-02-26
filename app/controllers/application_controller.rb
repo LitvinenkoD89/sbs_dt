650c4b268c848954b1715e124a61e67904c1c30b
LitvinenkoD89 <torerolda@gmail.com>
2018-02-26 14:26:12 +0300

    img

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+  # skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
   # protect_from_forgery prepend: true
   # protect_from_forgery with: :null_session
   # protect_from_forgery except: :sign_in
