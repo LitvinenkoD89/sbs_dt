b0c74e40a07da34ae92b51863d1e7874ab3aff1e
LitvinenkoD89 <torerolda@gmail.com>
2018-03-01 11:07:10 +0300

    form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  # skip_before_action :verify_authenticity_token, only: [:create]
 
   def authenticate_active_admin_user!
     authenticate_user!
